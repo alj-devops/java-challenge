8675309ca07a708de6788282238170ff31bb2459
Tangi <tangui.favennec@gmail.com>
2020-03-01 18:23:27 +0900

    Change simple prints by slf4j

--- src/main/java/jp/co/axa/apidemo/controllers/v1/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/v1/EmployeeController.java
@@ -2,6 +2,7 @@ package jp.co.axa.apidemo.controllers.v1;
 
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.services.EmployeeService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
 
 import java.util.List;
 
@@ -20,13 +22,15 @@ import static jp.co.axa.apidemo.controllers.Paths.*;
 @RequestMapping(__ + V1 + __ + EMPLOYEES)
 public class EmployeeController {
     
+    private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
+    
     @Autowired
     private EmployeeService employeeService;
     
     @DeleteMapping(__ + "{" + ID + "}")
     public void deleteEmployee(@PathVariable(name = ID) final Long id) {
         employeeService.deleteEmployee(id);
-        System.out.println("Employee Deleted Successfully");
+        log.info("Employee Deleted Successfully");
     }
     
     @GetMapping(__ + "{" + ID + "}")
@@ -43,10 +47,10 @@ public class EmployeeController {
     @PostMapping()
     public void saveEmployee(final Employee employee) {
         employeeService.saveEmployee(employee);
-        System.out.println("Employee Saved Successfully");
+        log.info("Employee Saved Successfully");
     }
     
-    public void setEmployeeService(EmployeeService employeeService) {
+    public void setEmployeeService(final EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
     
