98ccf016d4a22884ec9eb3ac184bb713e2ae38b2
Tangi <tangui.favennec@gmail.com>
2020-03-05 17:23:49 +0900

    removed unused

--- src/main/java/jp/co/axa/apidemo/common/WiringContexts.java
+++
@@ -1,9 +0,0 @@
-package jp.co.axa.apidemo.common;
-
-public class WiringContexts {
-    
-    public static final String DEFAULT = "default";
-    public static final String VALIDATION = "validation";
-    
-    
-}
