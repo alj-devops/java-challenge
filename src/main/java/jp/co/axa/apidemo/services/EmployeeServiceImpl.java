6bb76223d69b9cc0efaad510eacd666612c92776
Sharath Vignesh <sharathvignesh08@gmail.com>
2020-03-23 20:35:48 +0900

    added checks on the services

--- src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package jp.co.axa.apidemo.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,11 @@ import java.util.Optional;
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
+    /**
+     * logger
+     */
+    private static Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
@@ -18,25 +25,93 @@ public class EmployeeServiceImpl implements EmployeeService{
         this.employeeRepository = employeeRepository;
     }
 
+    /**
+     * Retrieves list of employees
+     */
     public List<Employee> retrieveEmployees() {
-        List<Employee> employees = employeeRepository.findAll();
-        return employees;
+        try {
+            List<Employee> employees = employeeRepository.findAll();
+            return employees;
+        } catch (Exception e) {
+            logger.error("Error while retrieving employees :(");
+            logger.error("Message: " + e.getMessage());
+            return null;
+        }
     }
 
+    /**
+     * Retrieves an employee with a given id
+     */
     public Employee getEmployee(Long employeeId) {
-        Optional<Employee> optEmp = employeeRepository.findById(employeeId);
-        return optEmp.get();
+        try {
+            if(employeeId != null) {
+                Optional<Employee> optEmp = employeeRepository.findById(employeeId);
+                return optEmp.get();
+            } else {
+                logger.warn("Please pass a valid employeeid");
+                return null;
+            }
+        } catch (Exception e) {
+            logger.error("Error while retrieving employee" + employeeId);
+            logger.error("Message: " + e.getMessage());            
+            return null;
+        }
     }
 
+    /**
+     * Saves an employee
+     */
     public void saveEmployee(Employee employee){
-        employeeRepository.save(employee);
+        try {
+            if(employee == null) {
+                logger.warn("Please pass a valid employee");
+                return null;
+            } else if(employee != null) {
+                employeeRepository.save(employee);
+                logger.info("Employee saved successfully");
+            }
+        } catch(Exception e) {
+            logger.error("Error while saving employee");
+            logger.error("Message: " + e.getMessage());            
+            return null;
+        }
     }
 
+    /**
+     * Deletes an employee with a given id
+     */
     public void deleteEmployee(Long employeeId){
-        employeeRepository.deleteById(employeeId);
+        try {
+            if(employeeId == null) {
+                logger.warn("Please pass a valid employee id");
+                return null;
+            } else if(employeeId != null) {
+                employeeRepository.deleteById(employeeId);
+                logger.info("Employee deleted successfully");
+            }
+        } catch(Exception e) {
+            logger.error("Error while deleting employee");
+            logger.error("Message: " + e.getMessage());            
+            return null;
+        }
     }
 
+    /**
+     * Updates an employee
+     */
     public void updateEmployee(Employee employee) {
-        employeeRepository.save(employee);
+        try {
+            if(employee == null) {
+                logger.warn("Please pass a valid employee");
+                return null;
+            } else if(employee != null) {
+                employeeRepository.save(employee);
+                logger.info("Employee updated successfully");
+            }
+        } catch(Exception e) {
+            logger.error("Error while updating employee");
+            logger.error("Message: " + e.getMessage());            
+            return null;
+        }
     }
 }
